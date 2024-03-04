ace77152802c8783d987162753ddf2d5f9409828
Felipe <felipesanchezp24@gmail.com>
2024-03-04 16:35:42 -0300

    04-03-2024

--- views/Perfil/Contacto.js
+++ views/Perfil/Contacto.js
@@ -42,8 +42,58 @@ const Contacto = () => {
           <Icon name="reader" size={30} color="#4BAADD" />
         </TouchableOpacity>
       </View>
-
-
+      <ScrollView
+        style={styles.contenedor}
+        contentContainerStyle={styles.scrollContent}
+      >
+        <Text style={styles.textoUno}>Formulario de contacto</Text>
+        <Text style={styles.textoUno}>
+          Si presentas algún problema con la aplicación, alguna falla,
+          comentario o duda, contáctanos mediante este formulario y nos
+          comunicaremos a la brevedad contigo para resolver tus dudas.
+        </Text>
+        <TextInput
+          // ref={(input) => {
+          //   this.TextInput1 = input;
+          // }}
+          // onSubmitEditing={() => this.TextInput2.focus()}
+          blurOnSubmit={false}
+          placeholder="Asunto"
+          // onChangeText={(text) => setSubject(text)}
+          // value={subject}
+          style={styles.input}
+        />
+        <TextInput
+          // ref={(input) => {
+          //   this.TextInput2 = input;
+          // }}
+          blurOnSubmit={false}
+          placeholder="Mensaje"
+          // onChangeText={(text) => setBody(text)}
+          // value={body}
+          multiline
+          style={styles.inputMsj}
+          placeholderTextColor={"grey"}
+        />
+        {/* <Controller
+          control={control}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <TextInput
+              onBlur={onBlur}
+              onChangeText={value => setBotcheck(value)}
+              value={value}
+              style={{ height: 0, width: 0, opacity: 0 }} // Hide the input
+            />
+          )}
+          name="botcheck"
+          defaultValue=""
+        /> */}
+        <TouchableOpacity>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Enviar</Text>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
@@ -53,7 +103,17 @@ export default Contacto;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#4BAADD",
     padding: 5,
+    position: "relative",
+  },
+  textoUno: {
+    color: "white",
+    marginBottom: 20,
+    textAlign: "center",
+    fontSize:15,
+    marginHorizontal:20,
+    marginBottom:50,
   },
   bottomMenu: {
     flexDirection: "row",
@@ -69,5 +129,49 @@ const styles = StyleSheet.create({
   },
   menuItem: {
     alignItems: "center",
+  },
+  input: {
+    backgroundColor: "white",
+    width: 300,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  contenedor: {
+    marginVertical: 20,
+    flexDirection: "column",
+    width: "100%",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    alignItems: "center",
+    backgroundColor: "#4BAADD",
+  },
+  icono: {
+    width: 80,
+    height: 80,
+  },
+  inputMsj: {
+    backgroundColor: "white",
+    width: 300,
+    height: 100,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: "#0074BD",
+    marginVertical: 8,
+    padding: 10,
+    width: 300,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
+  label: {
+    color: "grey",
   },
 });
--- views/Perfil/Preguntas.js
+++ views/Perfil/Preguntas.js
@@ -11,13 +11,17 @@ import { useNavigation } from "@react-navigation/native";
 
 const Preguntas = () => {
   const initialTexts = [
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
-    "Lorem ipsum dolor sit amet consectetur adipiscing elit sociis, duis malesuada velit praesent ridiculus ullamcorper quam himenaeos per, fermentum litora porta gravida sapien eleifend montes. Senectus vel sollicitudin viverra malesuada cras semper curabitur accumsan montes potenti elementum",
+    "¿Cómo funciona la aplicación para encontrar un profesional de la salud? \n\nLa aplicación utiliza un sistema de búsqueda que te permite filtrar profesionales de la salud por ubicación. Simplemente ingresa tus datos en la solicitud y un profesional se contactará contigo.",
+    "¿Qué tipo de profesionales de la salud están disponibles en la aplicación?\n\nEn la aplicación, puedes encontrar una amplia gama de profesionales de la salud, incluyendo médicos generales, especialistas, enfermeras, terapeutas y más.",
+    "¿Es seguro compartir información médica personal a través de la aplicación?\n\nSí, la aplicación utiliza medidas de seguridad avanzadas para proteger tu información médica personal. Todos los datos se cifran y se almacenan de forma segura en servidores protegidos.",
+    "¿Cómo puedo pagar por los servicios de salud a través de la aplicación? \n\nPuedes pagar por los servicios de salud a través de la aplicación utilizando métodos de pago seguros, como tarjetas de crédito o servicios de pago en línea o también en efectivo.",
+    "¿Qué debo hacer si tengo una emergencia médica mientras uso la aplicación? \n\nEn caso de una emergencia médica, por favor llama al número de emergencias de tu área o dirígete al hospital más cercano. La aplicación no debe usarse para emergencias médicas.",
+    "¿Qué sucede si no estoy satisfecho con la consulta con el profesional de la salud a través de la aplicación?\n\nSi no estás satisfecho con la consulta, por favor contáctanos para que podamos ayudarte a resolver cualquier problema o inquietud que puedas tener.",
+    "¿Puedo obtener recetas médicas a través de la aplicación?\n\nSí, algunos profesionales de la salud pueden proporcionar recetas médicas a través de la aplicación, según su criterio médico.",
+    "¿Qué debo hacer si tengo problemas técnicos mientras uso la aplicación durante una consulta?\n\nSi experimentas problemas técnicos durante una consulta, por favor comunícate con nuestro equipo de soporte técnico para recibir asistencia inmediata.",
+    "¿Cómo puedo verificar la credibilidad y las credenciales del profesional de la salud que elija a través de la aplicación?\n\nPuedes verificar la credibilidad y las credenciales del profesional de la salud revisando su perfil en la aplicación, donde encontrarás información sobre su formación, experiencia y certificaciones.",
+    "¿Qué tipo de preguntas médicas puedo hacer durante una consulta a través de la aplicación?\n\nDurante una consulta a través de la aplicación, puedes hacer preguntas sobre tus síntomas, condiciones médicas, tratamientos recomendados y cualquier otra preocupación relacionada con tu salud.",
+    "¿Hay algún costo asociado con registrarse en la aplicación como usuario?\n\nNo, el registro en la aplicación como usuario es gratuito. Sin embargo, pueden aplicarse cargos por los servicios de salud utilizados a través de la aplicación, dependiendo de las tarifas del profesional de la salud seleccionado.",
   ];
 
   const [visibleTextIndex, setVisibleTextIndex] = useState(null);
@@ -35,25 +39,25 @@ const Preguntas = () => {
           style={styles.menuItem}
           onPress={() => navigation.navigate("Principal")}
         >
-          <Icon name="home" size={30} color="#4BAADD" />
+          <Icon name="ios-home" size={30} color="#4BAADD" />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.menuItem}
           onPress={() => navigation.navigate("Perfil")}
         >
-          <Icon name="person" size={30} color="#0074BD" />
+          <Icon name="ios-person" size={30} color="#0074BD" />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.menuItem}
           onPress={() => navigation.navigate("Servicio")}
         >
-          <Icon name="medkit" size={30} color="#4BAADD" />
+          <Icon name="ios-medkit" size={30} color="#4BAADD" />
         </TouchableOpacity>
         <TouchableOpacity
           style={styles.menuItem}
           onPress={() => navigation.navigate("Personal")}
         >
-          <Icon name="reader" size={30} color="#4BAADD" />
+          <Icon name="ios-reader" size={30} color="#4BAADD" />
         </TouchableOpacity>
       </View>
       <ScrollView style={styles.contenedor}>
@@ -103,6 +107,7 @@ const styles = StyleSheet.create({
   contenedor: {
     flex: 1,
     width: "100%",
+    marginBottom:45,
   },
   button: {
     backgroundColor: "#0074BD",
